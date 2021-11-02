1a4198ae30fc300bb726e49bd39518fc98e6a364
Vasilii Pronin <letstalkaboutpork@gmail.com>
2021-11-02 17:43:56 +0300

    Initial Commit

---
+++ TimerApp/TimerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimerAppApp.swift
+//  TimerApp
+//
+//  Created by Василий Пронин on 02.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct TimerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
